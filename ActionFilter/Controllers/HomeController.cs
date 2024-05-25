b228284860cb17ba7169503662c3dddda0100143
Sourabh Raut <sourabh.raut@corecotechnologies.com>
2024-05-25 13:18:03 +0530

    Add project files.

---
+++ ActionFilter/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+using ActionFilter.Filter;
+using ActionFilter.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ActionFilter.Controllers
+{
+   
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [CustomActionFilter]
+        public IActionResult Privacy()
+        {
+            //
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ActionFilter/Filter/CustomActionFilter.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ActionFilter.Filter
+{
+    public class CustomActionFilter : Attribute, IActionFilter
+    {
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            //Do Logic check before Action Executing
+            Console.WriteLine("OnActionExecuting");
+        }
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            //Do Logic checks After Action Executed
+            Console.WriteLine("OnActionExecuted");
+
+        }
+    }
+}
